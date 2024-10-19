d1af70ce2ef2672828b2027cec97c76d7a4cfd15
The-Young-Programer <abduramonibrahim@gmail.com>
2024-10-19 12:22:56 +0100

    updated the association for management science

--- public/pth7zn95dekqgmu2ryfs3reatbnys5zxpah2skwhg1vu5bxwykc648f3d9nm7rej2zp4vvbnxg8y639mjgm57raz2/script.js
+++ public/pth7zn95dekqgmu2ryfs3reatbnys5zxpah2skwhg1vu5bxwykc648f3d9nm7rej2zp4vvbnxg8y639mjgm57raz2/script.js
@@ -50,6 +50,12 @@ function nextPrev(n) {
 //////////////////////////////////////////////////////////////
 
 
+
+
+
+
+// script.js
+// Form submission logic
 function submitForm() {
   // Create an object to hold form data
   const formData = {};
@@ -75,10 +81,10 @@ function submitForm() {
         // Construct the payment URL with pre-filled parameters
         const { first_name, last_name, email, mat_number, association } = formData;
         
-        // Append the form data as query parameters to the Paystack URL
-        const paymentUrl = `${data.paymentLink}?first_name=${encodeURIComponent(first_name)}&last_name=${encodeURIComponent(last_name)}&email=${encodeURIComponent(email)}&mat_number=${encodeURIComponent(mat_number)}&association=${encodeURIComponent(association)}&readonly=email,mat_number,association,first_name,last_name`;
+        // Add studentId to metadata for tracking
+        const paymentUrl = `${data.paymentLink}?first_name=${encodeURIComponent(first_name)}&last_name=${encodeURIComponent(last_name)}&email=${encodeURIComponent(email)}&mat_number=${encodeURIComponent(mat_number)}&association=${encodeURIComponent(association)}&metadata={"studentId":"${data.studentId}"}&readonly=email,mat_number,association,first_name,last_name`;
         
-        // Redirect to the Paystack payment page
+        // Redirect to Paystack payment page
         window.location.href = paymentUrl;
       } else {
         alert('Error submitting form. Please try again.');
@@ -89,8 +95,6 @@ function submitForm() {
       alert('Error submitting form. Please try again.');
     });
 }
-
-
 
 
 
